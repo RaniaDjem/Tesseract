047b7f03dee20f68e9a82fe498df723ab170555f
RaniaDjem <raniadjemdjem@gmail.com>
2024-01-19 11:40:30 +0100

    Update app1.py
    MAJ app1.py pour pouvoir afficher avec render_template

--- Service1/app1.py
+++ Service1/app1.py
@@ -1,15 +1,27 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+import os
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('upload.html')
 
 @app.route('/convert_to_text', methods=['POST'])
 def convert_image_to_text():
     try:
         uploaded_file = request.files['file']
-        # Ajoutez ici la logique pour traiter le fichier uploadé
-        # (conversion d'image en texte, etc.)
+        # Vérifiez si le nom de fichier est vide
+        if uploaded_file.filename == '':
+            return "Aucun fichier sélectionné."
+        
+        # Vérifiez si le dossier shared_data existe, sinon le crée
+        if not os.path.exists('/shared_data'):
+            os.makedirs('/shared_data')
+
         uploaded_file.save('/shared_data/image1.jpg')
         return "Traitement réussi"
+    
     except Exception as e:
         return f"Erreur : {str(e)}"
 
